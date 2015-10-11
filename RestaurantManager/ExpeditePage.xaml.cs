792123505c6d8c8b73dd3d0ada34581d1ae65d9c
spaghettisource <natureelement@gmail.com>
2015-10-11 01:24:20 -0700

    Homework 2

---
+++ RestaurantManager/ExpeditePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace RestaurantManager
+{
+    public sealed partial class ExpeditePage : Page
+    {
+        private void btnHome_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
--- RestaurantManager/MainPage.xaml.cs
+++ RestaurantManager/MainPage.xaml.cs
@@ -26,5 +26,15 @@ namespace RestaurantManager
         {
             this.InitializeComponent();
         }
+
+        private void btnOrders_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ExpeditePage));
+        }
+
+        private void btnSubmitOrder_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(OrderPage));
+        }
     }
 }
